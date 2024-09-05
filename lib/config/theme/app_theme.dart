c3c3eb0b8184375f71ba94db9ee909a5e796d84f
Luis Angel Ibañez Barbosa <chainblack9@gmail.com>
2024-09-05 06:55:44 -0500

    Agregado el tema para aplicación

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.indigo,
+  Colors.lime,
+  Colors.amber,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor > 0, 'Selected color must be less than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      );
+}
